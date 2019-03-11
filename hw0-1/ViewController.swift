4e020c6f0ae4abf2cfd53dc7a079cba936587a5a
Chang Tsai-Ting <tsai-tingchang@Tsai-Tings-Mac.local>
2019-03-11 09:38:48 -0700

    Initial Commit

---
+++ hw0-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw0-1
+//
+//  Created by Tsai-Ting, Chang on 3/11/19.
+//  Copyright © 2019 Tsai-Ting, Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
